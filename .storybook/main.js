d29e1ff1f3869eef398995893a9ad4e5fc05c3da
ironmanvim <vishalchiluveri@gmail.com>
2020-05-23 12:58:58 +0530

    changed a lot

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-storysource',
-    '@storybook/addon-knobs',
-    '@storybook/addon-a11y',
-  ],
+    stories: ['../src/Intro.stories.mdx', '../src/**/*.stories.(js|mdx)'],
+    addons: [
+        '@storybook/preset-create-react-app',
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/addon-storysource',
+        '@storybook/addon-knobs',
+        '@storybook/addon-a11y',
+        {
+            name: '@storybook/addon-docs',
+            options: {
+                configureJSX: true,
+            },
+        },
+    ],
 };
